cbb6c361d44ad181c07529263e5b60ab96d727c2
Fabricio Entringer Moreiera <moreira@gmail.com>
2021-11-10 00:25:52 +0100

    New project to Hacker Rank Challenges test

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import challenges.JavaIfElse;
+
+public class Main {
+    public static void main(String[] args) {
+        new JavaIfElse().execute(1);
+        new JavaIfElse().execute(2);
+        new JavaIfElse().execute(24);
+        new JavaIfElse().execute(4);
+        new JavaIfElse().execute(6);
+        new JavaIfElse().execute(7);
+    }
+}
